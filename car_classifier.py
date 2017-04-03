dad23a2a6a923d7db13e48278f95e5d7cc2df474
Raz Nissim <raznissim@gmail.com>
2017-04-03 18:04:59 +0300

    initial video pipeline.

--- car_classifier.py
+++ car_classifier.py
@@ -17,6 +17,8 @@ import pickle
 from sklearn.externals import joblib
 
 ### TODO: Tweak these parameters and see how the results change.
+from processing_utils import find_cars_in_image
+
 color_space = 'YCrCb'  # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
 orient = 8  # HOG orientations
 pix_per_cell = 8  # HOG pixels per cell
@@ -122,80 +124,51 @@ def train_model():
 
     return svc, X_scaler
 
-def convert_color(img, conv='RGB2YCrCb'):
-    if conv == 'RGB2YCrCb':
-        return cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
-    if conv == 'BGR2YCrCb':
-        return cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb)
-    if conv == 'RGB2LUV':
-        return cv2.cvtColor(img, cv2.COLOR_RGB2LUV)
 
 
-# Define a single function that can extract features using hog sub-sampling and make predictions
-def find_cars(img, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell, cell_per_block, spatial_size, hist_bins):
-    draw_img = np.copy(img)
-    img = img.astype(np.float32) / 255
 
-    img_tosearch = img[ystart:ystop, :, :]
-    ctrans_tosearch = convert_color(img_tosearch, conv='RGB2YCrCb')
-    if scale != 1:
-        imshape = ctrans_tosearch.shape
-        ctrans_tosearch = cv2.resize(ctrans_tosearch, (np.int(imshape[1] / scale), np.int(imshape[0] / scale)))
 
-    ch1 = ctrans_tosearch[:, :, 0]
-    ch2 = ctrans_tosearch[:, :, 1]
-    ch3 = ctrans_tosearch[:, :, 2]
 
-    # Define blocks and steps as above
-    nxblocks = (ch1.shape[1] // pix_per_cell) - 1
-    nyblocks = (ch1.shape[0] // pix_per_cell) - 1
-    nfeat_per_block = orient * cell_per_block ** 2
-    # 64 was the orginal sampling rate, with 8 cells and 8 pix per cell
-    window = 64
-    nblocks_per_window = (window // pix_per_cell) - 1
-    cells_per_step = 2  # Instead of overlap, define how many cells to step
-    nxsteps = (nxblocks - nblocks_per_window) // cells_per_step
-    nysteps = (nyblocks - nblocks_per_window) // cells_per_step
+_windows = []
 
-    # Compute individual channel HOG features for the entire image
-    hog1 = get_hog_features(ch1, orient, pix_per_cell, cell_per_block, feature_vec=False)
-    hog2 = get_hog_features(ch2, orient, pix_per_cell, cell_per_block, feature_vec=False)
-    hog3 = get_hog_features(ch3, orient, pix_per_cell, cell_per_block, feature_vec=False)
 
-    for xb in range(nxsteps):
-        for yb in range(nysteps):
-            ypos = yb * cells_per_step
-            xpos = xb * cells_per_step
-            # Extract HOG for this patch
-            hog_feat1 = hog1[ypos:ypos + nblocks_per_window, xpos:xpos + nblocks_per_window].ravel()
-            hog_feat2 = hog2[ypos:ypos + nblocks_per_window, xpos:xpos + nblocks_per_window].ravel()
-            hog_feat3 = hog3[ypos:ypos + nblocks_per_window, xpos:xpos + nblocks_per_window].ravel()
-            hog_features = np.hstack((hog_feat1, hog_feat2, hog_feat3))
+# TODO optimize the hog process like in find_cars
+def process_image(image, model, X_scaler):
+    draw_image = np.copy(image)
+    image_for_classification = image.astype(np.float32) / 255
 
-            xleft = xpos * pix_per_cell
-            ytop = ypos * pix_per_cell
+    window_sizes = [48, 64, 96, 128]
+    global _windows
+    if len(_windows) == 0:
+        for size in window_sizes:
+            _windows.extend(slide_window(image, x_start_stop=[None, None], y_start_stop=[400, 657],
+                                         xy_window=(size, size), xy_overlap=(0.7, 0.7)))
+        print("Done initializing windows.")
 
-            # Extract the image patch
-            subimg = cv2.resize(ctrans_tosearch[ytop:ytop + window, xleft:xleft + window], (64, 64))
+    hot_windows = search_windows(image_for_classification, _windows, model, X_scaler,
+                                 color_space=color_space,
+                                 spatial_size=spatial_size, hist_bins=hist_bins,
+                                 orient=orient, pix_per_cell=pix_per_cell,
+                                 cell_per_block=cell_per_block,
+                                 hog_channel=hog_channel, spatial_feat=spatial_feat,
+                                 hist_feat=hist_feat, hog_feat=hog_feat)
 
-            # Get color features
-            spatial_features = bin_spatial(subimg, size=spatial_size)
-            hist_features = color_hist(subimg, nbins=hist_bins)
+    heat = np.zeros_like(image[:, :, 0]).astype(np.float)
+    # Add heat to each box in box list
+    heat = add_heat(heat, hot_windows)
 
-            # Scale features and make a prediction
-            test_features = X_scaler.transform(
-                np.hstack((spatial_features, hist_features, hog_features)).reshape(1, -1))
-            # test_features = X_scaler.transform(np.hstack((shape_feat, hist_feat)).reshape(1, -1))
-            test_prediction = svc.predict(test_features)
+    # Apply threshold to help remove false positives
+    heat = apply_threshold(heat, 5)
 
-            if test_prediction == 1:
-                xbox_left = np.int(xleft * scale)
-                ytop_draw = np.int(ytop * scale)
-                win_draw = np.int(window * scale)
-                cv2.rectangle(draw_img, (xbox_left, ytop_draw + ystart),
-                              (xbox_left + win_draw, ytop_draw + win_draw + ystart), (0, 0, 255), 6)
+    # Visualize the heatmap when displaying
+    heatmap = np.clip(heat, 0, 255)
 
-    return draw_img
+    # Find final boxes from heatmap using label function
+    labels = label(heatmap)
+    draw_image = draw_labeled_bboxes(draw_image, labels)
+    return draw_image
+
+
 
 
 if __name__ == "__main__":
@@ -214,78 +187,21 @@ if __name__ == "__main__":
         print("Used model from training.")
 
 
-    ystart = 400
-    ystop = 750
-    scale = 2
-
+    # ystart = 400
+    # ystop = 750
+    # scale = 2
+    #
 
     # out_img = find_cars(draw_image, ystart, ystop, scale, loaded_model, loaded_X_scaler, orient, pix_per_cell, cell_per_block, spatial_size,
     #                     hist_bins)
-
-    image = mpimg.imread('test_images/test6.jpg')
-    draw_image = np.copy(image)
-    image_for_classification = image.astype(np.float32) / 255
-
-    window_sizes = [48, 64, 96, 128]
-    windows = []
-    for size in window_sizes:
-        windows.extend(slide_window(image_for_classification, x_start_stop=[None, None], y_start_stop=[400,657],
-                                                  xy_window=(size, size), xy_overlap=(0.7, 0.7)))
-
-
-    hot_windows = search_windows(image_for_classification, windows, loaded_model, loaded_X_scaler, color_space=color_space,
-                                 spatial_size=spatial_size, hist_bins=hist_bins,
-                                 orient=orient, pix_per_cell=pix_per_cell,
-                                 cell_per_block=cell_per_block,
-                                 hog_channel=hog_channel, spatial_feat=spatial_feat,
-                                 hist_feat=hist_feat, hog_feat=hog_feat)
-
-
-    heat = np.zeros_like(image[:, :, 0]).astype(np.float)
-    # Add heat to each box in box list
-    heat = add_heat(heat, hot_windows)
-
-    # Apply threshold to help remove false positives
-    heat = apply_threshold(heat, 2)
-
-    # Visualize the heatmap when displaying
-    heatmap = np.clip(heat, 0, 255)
-
-    # Find final boxes from heatmap using label function
-    labels = label(heatmap)
-    draw_img = draw_labeled_bboxes(np.copy(image), labels)
-
-    # window_img = draw_boxes(draw_image, hot_windows, color=(0, 0, 255), thick=2)
-    # cv2.imwrite("output_images/window_test5.jpg", cv2.cvtColor(draw_img, cv2.COLOR_RGB2BGR))
-    cv2.imwrite("output_images/window_test6.jpg", cv2.cvtColor(draw_img,cv2.COLOR_RGB2BGR))
+    for i in range(1,7):
+        image = mpimg.imread('test_images/test' + str(i) + '.jpg')
+        # image_with_cars = process_image(image, loaded_model, loaded_X_scaler)
+        image_with_cars = find_cars_in_image(image, 400, 657, 1, loaded_model, loaded_X_scaler, orient, pix_per_cell, cell_per_block, spatial_size, hist_bins, color_space)
+        cv2.imwrite('output_images/window_test' + str(i) + '.jpg', cv2.cvtColor(image_with_cars,cv2.COLOR_RGB2BGR))
 
 
 
-
-    # y_start_stop = [400, 700]  # Min and max in y to search in slide_window()
-    # # Check the prediction time for a single sample
-    # t = time.time()
-
-    # Uncomment the following line if you extracted training
-    # data from .png images (scaled 0 to 1 by mpimg) and the
-    # image you are searching is a .jpg (scaled 0 to 255)
-    # image = image.astype(np.float32)/255
-    #
-    # windows = slide_window(image, x_start_stop=[None, None], y_start_stop=y_start_stop,
-    #                        xy_window=(80, 80), xy_overlap=(0.5, 0.5))
-    #
-    # hot_windows = search_windows(image, windows, loaded_model, loaded_X_scaler, color_space=color_space,
-    #                              spatial_size=spatial_size, hist_bins=hist_bins,
-    #                              orient=orient, pix_per_cell=pix_per_cell,
-    #                              cell_per_block=cell_per_block,
-    #                              hog_channel=hog_channel, spatial_feat=spatial_feat,
-    #                              hist_feat=hist_feat, hog_feat=hog_feat)
-    #
-    # window_img = draw_boxes(draw_image, hot_windows, color=(0, 0, 255), thick=6)
-    #
-    # # plt.imshow(window_img)
-    # window_img = cv2.cvtColor(window_img, cv2.COLOR_RGB2BGR)
-    # cv2.imwrite("output_images/window_img_from_training.jpg", window_img)
 
 
 
---
+++ pipeline.py
@@ -0,0 +1,103 @@
+import pickle
+import numpy as np
+from moviepy.video.io.VideoFileClip import VideoFileClip
+
+from feature_extraction_utils import slide_window, search_windows, add_heat, apply_threshold, draw_labeled_bboxes
+from scipy.ndimage.measurements import label
+
+#global parameters used for both training and processing images
+from processing_utils import find_cars
+
+color_space = 'YCrCb'  # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
+orient = 8  # HOG orientations
+pix_per_cell = 8  # HOG pixels per cell
+cell_per_block = 2  # HOG cells per block
+hog_channel = "ALL"  # Can be 0, 1, 2, or "ALL"
+spatial_size = (32, 32)  # Spatial binning dimensions
+hist_bins = 48  # Number of histogram bins
+spatial_feat = True  # Spatial features on or off
+hist_feat = True  # Histogram features on or off
+hog_feat = True  # HOG features on or off
+window_sizes = [48, 64, 96, 128]
+
+svc = None
+X_scaler = None
+_windows = []
+# def process_image(image):
+#     global _windows
+#
+#     draw_image = np.copy(image)
+#     image_for_classification = image.astype(np.float32) / 255
+#
+#     if len(_windows) == 0:
+#         for size in window_sizes:
+#             _windows.extend(slide_window(image, x_start_stop=[None, None], y_start_stop=[400, 657],
+#                                          xy_window=(size, size), xy_overlap=(0.7, 0.7)))
+#         print("Done initializing windows.")
+#
+#     hot_windows = search_windows(image_for_classification, _windows, svc, X_scaler,
+#                                  color_space=color_space,
+#                                  spatial_size=spatial_size, hist_bins=hist_bins,
+#                                  orient=orient, pix_per_cell=pix_per_cell,
+#                                  cell_per_block=cell_per_block,
+#                                  hog_channel=hog_channel, spatial_feat=spatial_feat,
+#                                  hist_feat=hist_feat, hog_feat=hog_feat)
+#
+#     heat = np.zeros_like(image[:, :, 0]).astype(np.float)
+#     # Add heat to each box in box list
+#     heat = add_heat(heat, hot_windows)
+#
+#     # Apply threshold to help remove false positives
+#     heat = apply_threshold(heat, 5)
+#
+#     # Visualize the heatmap when displaying
+#     heatmap = np.clip(heat, 0, 255)
+#
+#     # Find final boxes from heatmap using label function
+#     labels = label(heatmap)
+#     draw_image = draw_labeled_bboxes(draw_image, labels)
+#     return draw_image
+
+ystart = 400
+ystop = 657
+def process_image(image):
+    draw_img = np.copy(image)
+    hot_windows = []
+    hot_windows.extend(
+        find_cars(image, ystart, ystop, 1, svc, X_scaler, orient, pix_per_cell, cell_per_block, spatial_size, hist_bins,
+                  color_space=color_space))
+    hot_windows.extend(
+        find_cars(image, ystart, ystop, 1.5, svc, X_scaler, orient, pix_per_cell, cell_per_block, spatial_size, hist_bins,
+                  color_space=color_space))
+    hot_windows.extend(
+        find_cars(image, ystart, ystop, 2, svc, X_scaler, orient, pix_per_cell, cell_per_block, spatial_size, hist_bins,
+                  color_space=color_space))
+
+    heat = np.zeros_like(image[:, :, 0]).astype(np.float)
+    # Add heat to each box in box list
+    heat = add_heat(heat, hot_windows)
+
+    # Apply threshold to help remove false positives
+    heat = apply_threshold(heat, 3)
+
+    # Visualize the heatmap when displaying
+    heatmap = np.clip(heat, 0, 255)
+
+    # Find final boxes from heatmap using label function
+    labels = label(heatmap)
+    draw_image = draw_labeled_bboxes(draw_img, labels)
+    return draw_image
+
+
+if __name__ == "__main__":
+
+    svc = pickle.load(open("model/model_0.9899.sav", 'rb'))
+    X_scaler = pickle.load(open('model/X_scaler.pkl', 'rb'))
+    print("Loaded model and scaler from file.")
+    video = 'project_video'
+    white_output = '{}_test_1.mp4'.format(video)
+    clip1 = VideoFileClip('{}.mp4'.format(video)).subclip(31, 44)
+    white_clip = clip1.fl_image(process_image)  # NOTE: this function expects color images!!
+    white_clip.write_videofile(white_output, audio=False)
+
+
